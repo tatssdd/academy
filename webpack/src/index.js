19c65ac0b29b53adf46e61ce5ae723ea82cdcc2e
tatssdd <spb.b.yandex@gmail.com>
2025-04-04 22:09:30 +0400

    webpack prj

---
+++ webpack/src/index.js
@@ -0,0 +1,7 @@
+function component() {
+    const element = document.createElement('div');
+    element.innerHTML = ['Hello', 'webpack'].join(' ');
+    return element;
+}
+
+document.body.appendChild(component());
